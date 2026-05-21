3b64446e5b3c8fc7bd6a58ccface881a75141f87
Pablo07-22 <juanpablonavasacosta22@gmail.com>
2026-05-21 17:12:51 -0500

    Primer comit, se establecio un html y css base y de crearon variables en js, ya se tiene idea

---
+++ App.js
@@ -0,0 +1,21 @@
+let rutaRegistrada = []
+let idEditando = null
+
+const botonRuta = document.getElementById('botonRuta')
+const formulario = document.getElementById('formulario')
+const ruta = document.getElementById('ruta')
+const conductor = document.getElementById('conductor')
+const hora = document.getElementById('hora')
+const ciudad = document.getElementById('ciudad')
+const Rutas = document.getElementById('Rutas')
+
+let estudiantes = []
+let idEstudianteEditando = null
+
+const btnEstudiante = document.getElementById('btnEstudiante')
+const formularioEstudiante = document.getElementById('formularioEstudiante')
+const estudiante = document.getElementById('estudiante')
+const email = document.getElementById('email')
+const estudiantesRegistrados = document.getElementById('estudiantesRegistrados')
+
+
